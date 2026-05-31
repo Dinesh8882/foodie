3c708a3205cd2fbfa0868d8d1b7deff67ff8e69c
dinesh8882 <dineshmahor0987@gmail.com>
2026-05-31 16:04:22 +0545

    forth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import Sidebar from './shared/Sidebar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Loading from './shared/LoadingPage';
 
 const Menu = React.lazy(() => import("./pages/Menu"));
 const Orders = React.lazy(() => import("./pages/Orders"));
@@ -10,24 +9,29 @@ const Favorites = React.lazy(() => import("./pages/Favorites"));
 const Profile = React.lazy(() => import("./pages/Profile"))
 const Settings = React.lazy(() => import("./pages/Settings"))
 
+import Loading from './shared/LoadingPage'
+
 function App() {
+
   return (
     <div className='flex'>
       <Sidebar />
+
       <Routes>
         <Route path='/' element={<Home />} />
+
         <Route path='/menu' element={
           <Suspense fallback={<Loading />}>
             <Menu />
           </Suspense>
         } />
-
         <Route path='/orders' element={<Orders />} />
         <Route path='/favorites' element={<Favorites />} />
         <Route path='/profile' element={<Profile />} />
         <Route path='/settings' element={<Settings />} />
+
       </Routes>
-    </div>
+    </div >
   );
 }
 
