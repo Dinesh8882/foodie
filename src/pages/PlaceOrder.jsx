7b29458bde14f08ed057c5b2dbbd87f938fd7719
dinesh8882 <dineshmahor0987@gmail.com>
2026-06-09 16:48:38 +0545

    Added progress page

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,14 +1,15 @@
 import React, { useContext } from 'react'
 import Wrapper from '../shared/Wrapper'
 import { SidbarContext } from '../context/SidbarContext'
+import Progress from '../shared/Progress'
 
 function PlaceOrder() {
-    
-  return (
-    <Wrapper>
-      helo
-    </Wrapper>
-  )
+
+    return (
+        <>
+            <Progress title="Place Order" path="" />
+        </>
+    )
 }
 
 export default PlaceOrder
--- src/shared/Progress.jsx
+++ src/shared/Progress.jsx
@@ -1,26 +1,42 @@
 import React from 'react'
-import { FiSettings } from "react-icons/fi";
+import { FiArrowLeft, FiSettings } from "react-icons/fi";
 
 import Wrapper from './Wrapper'
+import { useNavigate, useNavigation } from 'react-router-dom';
 
-function Progress({title}) {
+function Progress({ title }) {
+    const navigate = useNavigate()
+
+    
     return (
         <Wrapper>
-
             <div className="flex flex-col items-center justify-center min-h-[60vh] px-6 text-center">
-                <div className="p-4 rounded-full bg-gray-100 mb-4">
+
+
+
+                <div className="p-4 rounded-full bg-gray-100">
                     <FiSettings className="text-5xl text-gray-600 animate-spin-slow" />
                 </div>
 
-                <h1 className="text-2xl font-bold text-gray-800 mb-2">
-                    Settings Coming Soon
+                <h1 className="mt-4 text-2xl font-bold text-gray-800">
+                    Work in Progress
                 </h1>
 
-                <p className="max-w-md text-gray-600">
-                    We're currently working on the {title} page to bring you a better
-                    experience. This section will be available soon.
+                <p className="max-w-md mt-2 text-gray-600">
+                    We're currently working on the <span className="font-medium">{title}</span> page
+                    to bring you a better experience. This section will be available soon.
                 </p>
+                <div className='mt-3'>
+                    <button
+                        onClick={() => navigate(-1)}
+                        className="flex items-center cursor-pointer gap-2 px-4 py-1 bg-black text-white rounded-lg hover:opacity-90 transition"
+                    >
+                        <FiArrowLeft />
+                        Go Back
+                    </button>
+                </div>
             </div>
+
         </Wrapper>
     )
 }
