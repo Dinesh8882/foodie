cdbeaa88fc5e3a2c756553b8650db6735834f40b
dinesh8882 <dineshmahor0987@gmail.com>
2026-08-09 01:22:10 +0545

    fixing error

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -3,7 +3,6 @@ import Wrapper from '../shared/Wrapper'
 import OrderTabs from '../features/orders/component/OrderTabs'
 import OrderCard from '../features/orders/component/OrderCard'
 import SectionHeading from '../shared/SectionHeading'
-import OrderDetails from '../features/orders/component/OrderDetails'
 import useToggle from '../shared/hooks/useToggle'
 import useOrders from '../features/orders/hook/useOrders'
 
@@ -18,7 +17,6 @@ function Orders() {
   return (
     <Wrapper>
       <div className=''>
-        {/* <OrderDetails toggle={toggle} isOpen={isOpen} /> */}
         <SectionHeading title="My Orders" varient="orders" />
         <p className='text-gray-500 mb-4'>View and track all your orders in one place.</p>
         <OrderTabs />
